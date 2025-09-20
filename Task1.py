9b7b9f3afcba54440cc8104db01914ae5c627993
Himanshu Katwal <himanshukatwalnew@gmail.com>
2025-09-21 00:32:25 +0530

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,11 +1,11 @@
-Idea: Exploratory Data Analysis (EDA) on Retail Sales Data
+"""Idea: Exploratory Data Analysis (EDA) on Retail Sales Data
 
 
 Description:
 
 In this project, you will work with a dataset containing information about retail sales. The goal is
 to perform exploratory data analysis (EDA) to uncover patterns, trends, and insights that can
-help the retail business make informed decisions.
+help the retail business make informed decisions."""
 
 # --- Import Libraries ---
 import pandas as pd
